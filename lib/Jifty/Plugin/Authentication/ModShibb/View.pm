2d6fa00d4b12b3df3c96fd29e7cb3675f0bef67e
Yves Agostini <agostini@univ-metz.fr>
2010-10-13 21:45:21 +0200

    add a shibboleth test page for debugging sp/idp config

--- lib/Jifty/Plugin/Authentication/ModShibb/View.pm
+++ lib/Jifty/Plugin/Authentication/ModShibb/View.pm
@@ -18,4 +18,27 @@ template '/shibb_missing_attribute' => page {
     outs _("Your Identity Provider don't provide a required attribute for this application.")
 };
 
+template '/shibb_test' => page {
+    my ($plugin)  = Jifty->find_plugin('Jifty::Plugin::Authentication::ModShibb');
+    my %env = %ENV;
+    #eval { %env = Jifty->web->request->env() };
+    my $remote_user = $env{'REMOTE_USER'};
+    strong {'REMOTE_USER'}; outs ' : '.$remote_user;
+    br {};
+    return if !$remote_user;
+    br {};
+    outs ('Required attributes :');
+    ul {
+        foreach my $val (@{$plugin->shibb_mandatory} ) {
+        li { strong { $val}; outs ' : '.$env{$val} };
+        };
+    };
+    outs ('Full ENV values :');
+    ul {
+        foreach my $val (sort keys %env) {
+        li { strong { $val}; outs ' : '.$env{$val} };
+        };
+    };
+};
+
 1;
